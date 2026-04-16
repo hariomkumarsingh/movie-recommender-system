105a61725a76ca3d52d3f0cf21b0625db11246d6
Hariom Kumar Singh <mrhariom9102060357@gmail.com>
2026-04-16 16:11:48 +0530

    Initial clean commit

---
+++ app.py
@@ -0,0 +1,54 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import requests
+def fetch(movie_id):
+    response=requests.get('https://api.themoviedb.org/3/movie/{}?api_key=dc864ac8630e1c25b05a82b9b2b46684&language=en-us'.format(movie_id))
+    data=response.json()
+    return "https://image.tmdb.org/t/p/w500/"+data['poster_path']
+
+
+def recommend(movie_name):
+    movie_index = movie[movie['title'] == movie_name].index[0]
+    distances = similarity[movie_index]
+
+    movie_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+
+    recommended_movies = []
+    recommended_movies_poster=[]
+
+
+    for i in movie_list:
+        movie_id=movie.iloc[i[0]].movie_id
+        recommended_movies.append(movie.iloc[i[0]].title)
+        recommended_movies_poster.append(fetch(movie_id))
+
+    return recommended_movies, recommended_movies_poster
+
+movie_dick=pickle.load(open('movie_dick.pkl', 'rb'))
+movie=pd.DataFrame(movie_dick)
+
+similarity=pickle.load(open('similarity.pkl', 'rb'))
+
+st.title('movie recommender system')
+selected_movie_name = st.selectbox('How would you like to contacted?', movie['title'].values)
+if st.button('Recommend'):
+    name, posters = recommend(selected_movie_name)
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+        st.text(name[0])
+        st.image(posters[0])
+    with col2:
+        st.text(name[1])
+        st.image(posters[1])
+    with col3:
+        st.text(name[2])
+        st.image(posters[2])
+    with col4:
+        st.text(name[3])
+        st.image(posters[3])
+    with col5:
+        st.text(name[4])
+        st.image(posters[4])
